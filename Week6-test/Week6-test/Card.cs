2224f2d208ed246982b10841f8add4c295a96ce7
jel5000 <jellemerberg@gmail.com>
2019-10-31 13:51:35 +0100

    V 0.5 Voor de 100e keer
    Gefixte merge

---
+++ Week6-test/Week6-test/Card.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+
+namespace Memory_game_menuscreen
+{
+    class Card
+    {
+        private ImageSource front, back;
+        private bool clicked, visible;
+
+        public Card(ImageSource frontOfCard)
+        {
+            back = new BitmapImage(new Uri("driver cards/hidden.jpg", UriKind.Relative));
+            clicked = false;
+            visible = true;
+            front = frontOfCard;
+        }
+
+        public void Clicked()
+        {
+            clicked = true;
+        }
+
+        public void FlipToBack()
+        {
+            clicked = false;
+        }
+
+        public bool Clickable()
+        {
+            if (clicked || !visible)
+            {
+                return false;
+            }
+            else
+            {
+                return true;
+            }
+        }
+        public void MakeInvisible()
+        {
+            visible = false;
+        }
+        public ImageSource GetImage()
+        {
+            return front;
+        }
+
+        public ImageSource Show()
+        {
+            if (visible)
+            {
+                if (clicked)
+                {
+                    return front;
+                }
+                else
+                {
+                    return back;
+                }
+            }
+            else
+            {
+                return null;
+            }
+        }
+    }
+}
