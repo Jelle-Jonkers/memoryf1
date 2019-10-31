8604597a480a6a550c9cdba67128a3d6726a0a88
jel5000 <jellemerberg@gmail.com>
2019-10-31 11:55:23 +0100

    Revert "0.5"
    This reverts commit 84ded09321e5226ffa9e5663de9100fc374a42f3.

--- Week6-test/Week6-test/MemoryGrid.cs
+++ Week6-test/Week6-test/MemoryGrid.cs
@@ -19,7 +19,6 @@ namespace Memory_game_menuscreen
         private List<Card> cards = new List<Card>();
         private int NrOfClickedCards = 0;
         private int previousCard;
-
         
         
         public MemoryGrid(Grid grid, int cols, int rows)
@@ -32,12 +31,7 @@ namespace Memory_game_menuscreen
             ShowCards();
         }
 
-        public MemoryGrid(int rows, int cols)
-        {
-            this.rows = rows;
-            this.cols = cols;
-        }
-
+        
         private void CardClick(object sender,MouseButtonEventArgs e)
         {
             if (NrOfClickedCards < 2)
