a9d498c1449680c6d482e0b6fdd0f9d58b34688b
jel5000 <jellemerberg@gmail.com>
2019-10-31 12:17:55 +0100

    V0.51
    Nu wel Menu er bij

---
+++ Memory game menuscreen/MainWindow.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+
+namespace Memory_game_menuscreen
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void ToOptionMenu(object sender, RoutedEventArgs e)
+        {
+            Menu menu = new Menu();
+            menu.SetMenu(menu);
+            this.Content = menu;
+        }
+        
+    }
+}
