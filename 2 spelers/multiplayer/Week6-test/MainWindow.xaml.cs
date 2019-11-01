a117a955ab0a7ef18d744c8caa808c56a4181392
larsvdk1301 <lars.van.de.kamp@student.nhlstenden.com>
2019-11-01 10:38:47 +0100

    multiplayer V 0.4

---
+++ 2 spelers/multiplayer/Week6-test/MainWindow.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+
+namespace Week6_test
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        const int rows = 4;
+        const int cols = 4;
+        public bool speler1, speler2;
+        MemoryGrid grid;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            grid = new MemoryGrid(MainGrid, cols, rows);
+        }
+
+    }      
+}
